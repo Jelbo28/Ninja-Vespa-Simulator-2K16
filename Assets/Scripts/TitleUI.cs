e7d888d436b1d1573b06c40e86e213f9fe578c62
Aram Joseph <ajoseph@mr.preble.com>
2016-03-29 10:28:35 -0400

    Title Scene and Credits

---
+++ Assets/Scripts/TitleUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleUI : MonoBehaviour
+{
+	public void Play()
+	{
+		//Debug.Log("Play");
+		Application.LoadLevel(1);
+	}
+	
+	public void Quit()
+	{
+		//Debug.Log("Quit");
+		Application.Quit();
+	}
+}
