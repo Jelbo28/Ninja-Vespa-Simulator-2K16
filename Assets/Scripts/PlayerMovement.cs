00d395c78446ef6a05f9d140cdce8f979ffc4b92
Aram Joseph <ajoseph@mr.preble.com>
2016-03-31 10:21:27 -0400

    Animating Cutscene #1
    Finished title menu

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+   public Rigidbody2D rb;
+    public float speed = 120f;
+    
+	
+	void FixedUpdate ()
+    {
+        rb.GetComponent<Rigidbody2D>();
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left * speed);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * speed);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(Vector3.up * 300);
+        }
+	}
+}
