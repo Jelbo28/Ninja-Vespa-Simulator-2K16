7c5c7bdd116615078fadb3aa337af7ce9b6adac0
Aram Joseph <ajoseph@mr.preble.com>
2016-04-01 08:18:47 -0400

    Latest Compilation

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField]
+    float distance = 75f;
+
+    [SerializeField]
+    Vector2 RayAngle = new Vector2(1, 1);
+
+    void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, RayAngle, distance);
+
+        Debug.DrawRay(transform.localPosition, RayAngle, Color.yellow);
+        if (hit.collider.tag == "Player")
+        {
+            Debug.Log("This thing is the player I think");
+        }
+    }	
+}
---
+++ Assets/Scripts/ThrowingStars.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowingStars : MonoBehaviour {
+    [SerializeField]
+    GameObject NStar;
+    [SerializeField]
+    GameObject FirePoint;
+
+    public Rigidbody2D rb;
+    public float speed;
+
+	void FixedUpdate ()
+    {
+        speed = 100;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(NStar, FirePoint.transform.position, FirePoint.transform.rotation); 
+        }
+	}
+}
