1f7890d90e4598c6711b9a9e15ab4fcb1735ca6f
Aram Joseph <ajoseph@utc4me.org>
2016-04-06 19:53:14 -0400

    Reattached Shit

--- Assets/Scripts/TransitionPoint.cs
+++ Assets/Scripts/TransitionPoint.cs
@@ -3,6 +3,18 @@ using System.Collections;
 
 public class TransitionPoint : MonoBehaviour
 {
+    [SerializeField]
+    AudioSource[] sounds;
+    public AudioSource engine;
+    public AudioSource honk;
+
+    void start()
+    {
+        sounds = GetComponents<AudioSource>();
+        engine = sounds[0];
+        honk = sounds[1];
+    }
+
     public void CutOne()
     {
 		Application.LoadLevel(1);
@@ -16,5 +28,15 @@ public class TransitionPoint : MonoBehaviour
     public void Disable()
     {
         this.gameObject.SetActive(false);
+    }
+
+    public void CutAudio()
+    {
+        this.gameObject.GetComponent<AudioSource>().enabled = false;
+    }
+
+    public void Honk()
+    {
+        honk.Play();
     }
 }
