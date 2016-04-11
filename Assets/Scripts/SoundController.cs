bb103e5abe4b7d2c7d78a7ffd5e860036f9e4ec0
Aram Joseph <ajoseph@mr.preble.com>
2016-04-11 10:19:35 -0400

    Ni\oice

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -7,12 +7,17 @@ public class SoundController : MonoBehaviour
     AudioSource[] sounds;
     public AudioSource engine;
     public AudioSource honk;
+	public AudioSource angry;
+	public AudioSource intrigued;
+	public AudioSource summon;
 
     void start()
     {
-        sounds = GetComponents<AudioSource>();
-        engine = sounds[0];
-        honk = sounds[1];
+		/*
+		sounds = GetComponents<AudioSource>();
+		engine = sounds[0];
+		honk = sounds[1];
+		*/
     }
 
     public void CutAudio()
@@ -24,4 +29,25 @@ public class SoundController : MonoBehaviour
     {
         honk.Play();
     }
+
+	public void Angry()
+	{
+		angry.Play();
+	}
+
+	public void Intrigued()
+	{
+		intrigued.Play();
+	}
+
+	public void StopBro()
+	{
+		angry.enabled = false;
+		intrigued.enabled = false;
+	}
+
+	public void Summon()
+	{
+		summon.Play ();
+	}
 }
