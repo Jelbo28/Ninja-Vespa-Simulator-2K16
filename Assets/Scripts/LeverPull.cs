a1dabce800bbecf6b429ab7ce05fc50e59bcd338
Aram Joseph <ajoseph@utc4me.org>
2016-04-11 21:24:24 -0400

    Finished Outlining final scenes

---
+++ Assets/Scripts/LeverPull.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeverPull : MonoBehaviour
+{
+    [SerializeField]
+    GameObject brother;
+    [SerializeField]
+    bool toggle;
+    [SerializeField]
+    Sprite leverDown;
+    [SerializeField]
+    GameObject mud;
+
+    void OnTriggerEnter2D(Collider other)
+    {
+        if (toggle == true /*other.tag == "Shuriken"*/)
+        {
+            
+        }
+    }
+
+    void Update()
+    {
+        if (toggle == true)
+        {
+            brother.GetComponent<PolygonCollider2D>().enabled = false;
+            gameObject.GetComponent<SpriteRenderer>().sprite = leverDown;
+            mud.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 255);
+        }
+    }
+}
